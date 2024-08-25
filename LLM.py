2b3a39865c036dbf3b9eb110ac3c936c7e6c596b
Andrii Kozin <andriikozin@microsoft.com>
2024-08-25 13:56:04 +0200

    An attempt to change the LLM model

--- LLM.py
+++ LLM.py
@@ -11,7 +11,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
 class OllamaLLM:
-    def __init__(self, model_name='gemma2'):
+    def __init__(self, model_name='gemma2:2b'):
         self.model = Ollama(
             base_url='http://localhost:11434',
             model=model_name
@@ -20,7 +20,7 @@ class OllamaLLM:
         self.vectorstore = Chroma(
             "wiki_store", 
             self.oembed,
-            "./chroma_langchain_db")
+            "./chroma_langchain_db_yaml")
         self.text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200)
@@ -53,11 +53,14 @@ class OllamaLLM:
         retriever = self.vectorstore.as_retriever()
         # 2. Incorporate the retriever into a question-answering chain.
         system_prompt = (
-            "You are an assistant for question-answering tasks. "
+            "You are an assistant for Traffic Management Topology definitions. "
             "Use the following pieces of retrieved context to answer "
-            "the question. If you don't know the answer, say that you "
-            "don't know. Use five sentences maximum and keep the "
-            "answer concise."
+            "the question. The information you have access to represents the "
+            "topology definitions per policy. servicedef.yaml is the Environment, "
+            "top level domain, weighted switch definition. While other files are "
+            "representing policies and their definitions. "
+            " If you don't know the answer, say that you "
+            "don't know. Keep the answer concise."
             "\n\n"
             "{context}"
         )
