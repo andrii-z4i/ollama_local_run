c4cb9ba6cd77fef61bf882bc1090ca83d769f3b8
Andrii Kozin <andriikozin@microsoft.com>
2024-08-25 21:41:00 +0200

    Extract embedding logic

--- run.py
+++ run.py
@@ -1,34 +1,37 @@
 import os
 from LLM import OllamaLLM
-from typing import Iterable
+from typing import Iterable, List
+
+from embeding import Embedding
 
 
-def get_markdown_files(directory) -> Iterable[str]:
+def enumerate_files(directory: str, extensions: List[str]) -> Iterable[str]:
     for filename in os.listdir(directory):
         full_path = os.path.join(directory, filename)
-        if filename.endswith(".md"):
+        # if filename ends with one of the extensions
+        if any([filename.endswith(ext) for ext in extensions]):
             yield full_path
         elif os.path.isdir(full_path):
-            yield from get_markdown_files(full_path)
+            yield from enumerate_files(full_path, extensions)
 
-def process_markdown_files(directory, llm: OllamaLLM):
+def process_files(directory: str, extensions: List[str], embedding: Embedding):
 
-    for md_file_path in get_markdown_files(directory):
-        processed_md = llm.load_markdown(md_file_path)
-        llm.submit_to_model(processed_md)
-        print(f"Processed {md_file_path}")
+    for file_for_processing in enumerate_files(directory, extensions):
+        processed_files = embedding.load_content_from_path(file_for_processing)
+        print(f"{file_for_processing} has been processed.") if len(processed_files) > 0 else print(f"{file_for_processing} has NOT been processed.")
     
 
 if __name__ == "__main__":
-    ollama = OllamaLLM()
+    embedding = Embedding()
+    ollama = OllamaLLM(embedding)
     wiki_path = "/Users/andriikozin/prj/ms/wiki"
-    tcs_wiki = "IdentityWiki"
+    tcs_wiki = "IdentityWiki/Services/Routing/TCS-(Traffic-Control-Service)"
     
     markdown_directory = '/'.join([wiki_path, tcs_wiki])
     
-    if ollama.is_vectorstore_empty():
+    if embedding.is_vectorstore_empty():
         print("Vectorstore is empty. Loading markdown files.")
-        process_markdown_files(markdown_directory, ollama)
+        process_files(markdown_directory, ['md'], embedding)
     
     # Enter into an interactive loop for conversation
     while True:
